fd548051fb54b9623810d646476e65b4c45c9496
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-02-13 20:36:21 +0100

    Tests for encryptWithSpacesAndPunctuation in VigenereCipher passed

--- src/test/java/michal/garnczarski/com/VigenereCipherTest.java
+++ src/test/java/michal/garnczarski/com/VigenereCipherTest.java
@@ -33,5 +33,28 @@ public class VigenereCipherTest {
 	public void encryptWithSpacesAndPunctuationTest() {
 		Assert.assertEquals("ABBC, CAAB.", vigenereCipher.encryptWithSpacesAndPunctuation("Baba, baba."));
 	}
+	
+	@Test
+	public void keyAEncryptWithSpacesAndPunctuationTest() {
+		vigenereCipher = new VigenereCipher("a", alphabet);
+		Assert.assertEquals("BABA, BABA.", vigenereCipher.encryptWithSpacesAndPunctuation("Baba, baba."));
+	}
+	
+	@Test
+	public void keyAaAEncryptWithSpacesAndPunctuationTest() {
+		vigenereCipher = new VigenereCipher("AaA", alphabet);
+		Assert.assertEquals("BABA, BABA.", vigenereCipher.encryptWithSpacesAndPunctuation("Baba, baba."));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void emptyAlphabetEncryptWithSpacesAndPunctuationTest() {
+		ArrayList<Character> alphabet = new ArrayList<Character>();
+		vigenereCipher = new VigenereCipher("cba", alphabet);
+	}
+	
+	@Test
+	public void emptyEncryptWithSpacesAndPunctuationTest() {
+		Assert.assertEquals("", vigenereCipher.encryptWithSpacesAndPunctuation(""));
+	}
 
 }
