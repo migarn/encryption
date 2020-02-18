946b2d37ef355c45b63cf09099a24411fc2ce088
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-02-18 18:34:36 +0100

    Alphabets added to Main

--- src/main/java/michal/garnczarski/com/Main.java
+++ src/main/java/michal/garnczarski/com/Main.java
@@ -1,9 +1,22 @@
 package michal.garnczarski.com;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 	static UIScanner uIScanner = new UIScanner();
+	static ArrayList<Character> latinAlphabetSigns = new ArrayList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+	static ArrayList<Character> polishAlphabetSigns = new ArrayList<Character>(Arrays.asList('A', '•', 'B', 'C', '∆', 'D', 'E', ' ', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '£', 'M', 'N', '—', 'O', '”', 'P', 'R', 'S', 'å', 'T', 'U', 'W', 'Y', 'Z', 'è', 'Ø'));
+	static ArrayList<Character> polishExtendedAlphabetSigns = new ArrayList<Character>(Arrays.asList('A', '•', 'B', 'C', '∆', 'D', 'E', ' ', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '£', 'M', 'N', '—', 'O', '”', 'P', 'Q', 'R', 'S', 'å', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'è', 'Ø'));
+	static Alphabet latinAlphabet = new Alphabet("Latin alphabet", latinAlphabetSigns);
+	static Alphabet polishAlphabet = new Alphabet("Polish alphabet", polishAlphabetSigns);
+	static Alphabet polishExtendedAlphabet = new Alphabet("Polish extended alphabet", polishExtendedAlphabetSigns);
+	static AlphabetsList alphabetsList = new AlphabetsList();
 
 	public static void main(String[] args) {
+		alphabetsList.addAlphabet(latinAlphabet);
+		alphabetsList.addAlphabet(polishAlphabet);
+		alphabetsList.addAlphabet(polishExtendedAlphabet);
 		System.out.println("This application enables encrypting and decrypting given text by means of two ciphers: Caesar cipher and Vigenere cipher.");
 		mainMenu();
 	}
